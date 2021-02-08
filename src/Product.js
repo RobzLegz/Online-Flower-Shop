7da1c626669b46d4e14683826f430bd4f9abb67e
Robzlegz <robzlegz@gmail.com>
2021-02-08 14:45:56 +0200

    styling products

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./Product.css";
 
-const Product = () => {
+const Product = ({title, price, description, image}) => {
     return (
         <div className="product">
-            product
+            <div className="productImageContainer">
+                <img src={image} alt="product image"/>
+            </div>
+            <div className="productDescription">
+                <h3>{title}</h3>
+                <h4>{price}€</h4>
+                <p>{description}</p>
+            </div>
         </div>
     );
 }

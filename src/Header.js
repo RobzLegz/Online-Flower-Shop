4e9773f3776c906d915b41e809031a09de31a6ec
Robzlegz <robzlegz@gmail.com>
2021-02-08 12:18:18 +0200

    installing packages

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Header.css";
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
 
 
 const Header = () => {
+
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if(window.scrollY > 100){
+            handleShow(true);
+        }else{
+            handleShow(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, []) 
+
     return (
-        <div className="header">
+        <div className={`header ${ show && "scrolledHeader"}`}>
             <h1 className="logo">Bēru vainagi</h1>
             <ul className="navLinks">
                 <li><a href="#">Ziedu Grozi</a></li>
-                <li><a href="#">Ienākt</a></li>
-                <li><a href="#">Grozs</a></li>
+                <li><a href="#"><PersonOutlineIcon /> Ienākt</a></li>
+                <li><a href="#"><ShoppingBasketOutlinedIcon/> Grozs</a></li>
             </ul>
         </div>
     );

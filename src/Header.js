836890510aa5451dd802aa1b546cc9ffe8593111
Robzlegz <robzlegz@gmail.com>
2021-02-08 11:45:03 +0200

    updating nav bar

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,10 @@ const Header = () => {
     return (
         <div className="header">
             <img src={Logo} alt=""/>
+            <ul className="navLinks">
+                <li><a href="#">Ziedu Grozi</a></li>
+                <li><a href="#">Grozs</a></li>
+            </ul>
         </div>
     );
 }

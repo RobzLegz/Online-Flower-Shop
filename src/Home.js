0f3dae90e2a29bcc7eb2f1378f67c9c33a4a69d5
Robzlegz <robzlegz@gmail.com>
2021-02-08 12:31:19 +0200

    header update

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Home.css";
 import Product from './Product';
+import HomeHeaderImage from "./resources/homeHeaderImage.jpg";
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home-header">
-                
+                <img className="homeHeaderImage" src={HomeHeaderImage} alt=""/>
             </div>
             <Product />
         </div>

ae17cd60a8b97858c70efb2c0c3d5b5396de4176
Robzlegz <robzlegz@gmail.com>
2021-02-08 11:04:51 +0200

    setting up home page

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css";
+import Product from './Product';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Product />
+        </div>
+    );
+}
+
+export default Home;

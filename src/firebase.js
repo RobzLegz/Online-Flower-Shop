8ec04b228fdacd9870b368a83e34110396798d36
Robzlegz <robzlegz@gmail.com>
2021-02-11 11:16:32 +0200

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCF2PWhoGnnGDlFaBog97bvu2xaWvUxj7s",
+    authDomain: "beruvainagi.firebaseapp.com",
+    projectId: "beruvainagi",
+    storageBucket: "beruvainagi.appspot.com",
+    messagingSenderId: "814475863638",
+    appId: "1:814475863638:web:af1c209b956849b7813df7",
+    measurementId: "G-16Z23Q5SJX"
+};

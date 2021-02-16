63a609d1ca2b38681ddbfef5b24ac542de307e35
Robzlegz <robzlegz@gmail.com>
2021-02-16 20:56:00 +0200

    styling login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { password, userName } from './constants';
 import "./LoginPage.css";
 
 const LoginPage = () => {
+
+    const history = useHistory();
+
+    const [usernameInput, setUsernameInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+
+    const checkLogin = (e) => {
+        e.preventDefault();
+        if(usernameInput === userName && passwordInput === password){
+            alert("working")
+        }else{
+            alert("Nepaireizs lietotājvārds vai parole")
+        }
+    }
+
     return (
         <div className="loginPage">
             <form>
-                <input type="text" placeholder="Lietotājvārds" />
-                <input type="password" placeholder="Parole" />
-                <button type="submit">Ieiet</button>
+                <input type="text" value={usernameInput} onChange={(e) => setUsernameInput(e.target.value)} placeholder="Lietotājvārds" />
+                <input type="password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} placeholder="Parole" />
+                <button type="submit" onClick={checkLogin}>Ieiet</button>
             </form>
         </div>
     )

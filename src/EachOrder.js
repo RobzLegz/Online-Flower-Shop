8c8d77a33d1b03a5a020f60dc04ee3935feb9998
Robzlegz <robzlegz@gmail.com>
2021-02-16 21:36:06 +0200

    adding order data

---
+++ src/EachOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./EachOrder.css";
+
+const EachOrder = () => {
+    return (
+        <div className="eachOrderContainer">
+            
+        </div>
+    )
+}
+
+export default EachOrder
--- src/OrdersPage.js
+++ src/OrdersPage.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import EachOrder from './EachOrder';
+import db from './firebase';
 import "./OrdersPage.css";
 
 const OrdersPage = () => {
+
+    const [allOrders, setAllOrders] = useState([]);
+
+    useEffect(() => {
+        db.collection("orders").orderBy("orderDate", "desc").onSnapshot((snapshot) => (
+            setAllOrders(snapshot.docs.map((doc) => doc.data()))
+        ))
+    }, []);
+
     return (
         <div className="ordersPage">
             <div className="allOrderContainer">
-                
+                {allOrders.map((order) => (
+                    <EachOrder
+                        key={order.id} 
+                        id={order.id} 
+                        address={order.address} 
+                        email={order.email} 
+                        name={order.name}
+                        phone={order.number}
+                        actualOrder={order.order}
+                        orderDate={order.orderDate}
+                        orderValue={order.orderValue}
+                    />
+                ))}
             </div>
         </div>
     )

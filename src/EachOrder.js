89af2881315ff08c8d18247fad0d362162bc4596
Robzlegz <robzlegz@gmail.com>
2021-02-16 21:41:18 +0200

    generating order data

--- src/EachOrder.js
+++ src/EachOrder.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./EachOrder.css";
 
-const EachOrder = () => {
+const EachOrder = ({address,email,name,phone,actualOrder,orderDate,orderValue}) => {
     return (
         <div className="eachOrderContainer">
-            
+            <div className="userInfo">
+                <p>{address}</p>
+                <small>{email}</small>
+                <h5>{name}</h5>
+                <p>{phone}</p>
+            </div>
+            <div className="orderDetails">
+                <h4>{orderValue}</h4>
+                
+            </div>
         </div>
     )
 }
